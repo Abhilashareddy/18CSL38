3c09f86304a570d6d4b2363fc7162e7c0f971725
Mohammed Rabil <rabil@techie.com>
2022-01-03 07:25:38 +0530

    Error Fix! ✅

--- 4. Infix to Postfix Converter/main.c
+++ 4. Infix to Postfix Converter/main.c
@@ -14,36 +14,41 @@
 
 char stack[MX];
 
-int top=-1;
+int top = -1;
 
 // Check stack underflow
-int isEmpty() {
-    if (top == -1) 
+int isEmpty()
+{
+    if (top == -1)
         return 1;
-    else 
+    else
         return 0;
 }
 
 // Check stack overflow
-int isFull() {
-    if (top == MX-1)
+int isFull()
+{
+    if (top == MX - 1)
         return 1;
-    else 
+    else
         return 0;
 }
 
 // Stack Push
-void push(char item){
+void push(char item)
+{
     // Check for overflow!
-    if (isFull()) 
+    if (isFull())
         printf("Error: Stack Overflow!\n");
     else
         stack[++top] = item;
 }
 
 // Stack Pop
-char pop() {
-    if (isEmpty()) {
+char pop()
+{
+    if (isEmpty())
+    {
         printf("Error: Invalid Parenthesis!\n");
         exit(1);
     }
@@ -52,82 +57,111 @@ char pop() {
 }
 
 // Precedence of opeartors
-int precedence(char op) {
+int precedence(char op)
+{
     switch (op)
     {
-        case '#': return 0;
-        case '(': return 1;
-        case '+':
-        case '-': return 2;
-        case '*':
-        case '/':
-        case '%': return 3;
-        case '^': return 4;
-        default:
-            printf("Error: Invalid Operator\n");
-            exit(1);
-            break;
+    case '#':
+        return 0;
+    case '(':
+        return 1;
+    case '+':
+    case '-':
+        return 2;
+    case '*':
+    case '/':
+    case '%':
+        return 3;
+    case '^':
+        return 4;
+    default:
+        printf("Error: Invalid Operator\n");
+        exit(1);
+        break;
     }
 }
 
 // Compute arithmetic operation
-int compute(char symbol, int op1, int op2) {
+int compute(char symbol, int op1, int op2)
+{
     switch (symbol)
     {
-        case '+': return op1+op2;
-        case '-': return op1-op2;
-        case '*': return op1*op2;
-        case '/': return op1/op2;
-        case '%': return op1%op2;
-        case '^': return pow(op1,op2);
-        default:  return 0;
+    case '+':
+        return op1 + op2;
+    case '-':
+        return op1 - op2;
+    case '*':
+        return op1 * op2;
+    case '/':
+        return op1 / op2;
+    case '%':
+        return op1 % op2;
+    case '^':
+        return pow(op1, op2);
+    default:
+        return 0;
     }
 }
 
-// Function to convert infix to postfix 
+// Function to convert infix to postfix
 // returns postfix string on success
-int Convert(char *infix, char *postfix) {
+int Convert(char *infix, char *postfix)
+{
     char ch; // holds char from infix
     // index variables
-    int i=0; // infix
-    int k=0; // postfix
+    int i = 0; // infix
+    int k = 0; // postfix
 
-    int co=0; // count Operators 
-    int ca=0; // count Operands
+    int co = 0; // count Operators
+    int ca = 0; // count Operands
 
     // for error handling purposes top element of stack will be '#'
     push('#');
 
-    while((ch = infix[i++]) != '\0') {
-        if( ch == '(' ) {
+    while ((ch = infix[i++]) != '\0')
+    {
+        if (ch == '(')
+        {
             push(ch); // Push to stack
-        } else if ( isalnum(ch) ) {
+        }
+        else if (isalnum(ch))
+        {
             postfix[k++] = ch; // insert char to postfix string
             ca++;
-        } else if ( ch == ')' ) {
+        }
+        else if (ch == ')')
+        {
             // insert char in stack till '(' is found
-            while(stack[top] != '(')
-                postfix[k++] = pop(); 
+            while (stack[top] != '(')
+                postfix[k++] = pop();
             pop(); // Now remove '(' from stack
-        } else {
+        }
+        else
+        {
             // if char in stack is of higher precedence than ch
             // insert to postfix
             co++;
-            while(precedence(stack[top]) >= precedence(ch) )
+            while (precedence(stack[top]) >= precedence(ch))
                 postfix[k++] = pop();
             push(ch); // Now push ch to stack
         }
     }
     // Until stack is empty
-    while(stack[top]!='#') {
+    while (stack[top] != '#')
+    {
         // check if there's any remaining '(' in stack
-        if (stack[top] == '(') {
+        if (stack[top] == '(')
+        {
             printf("Error: Invalid Parenthesis\n");
             exit(1);
-        } else if (co != ca-1) { // No of operands should be equal to (1 less than no. of operators)
+        }
+        else if (co != ca - 1)
+        { // No of operands should be equal to (1 less than no. of operators)
             printf("Invalid Expression\n");
             exit(1);
-        } else {
+        }
+        else
+        {
             // Now insert all operators remaining in stack to postfix
             postfix[k++] = pop();
         }
@@ -137,11 +171,11 @@ int Convert(char *infix, char *postfix) {
 }
 
 // Driver program
-void main() {
+void main()
+{
     char infix[100], postfix[100];
     printf("Enter infix expression: ");
     scanf("%s", infix);
     Convert(infix, postfix);
     printf("Result: %s\n", postfix);
 }
-
